0ecd0b62051433a3c6c51e7c4f89c4de23395589
Andreas Taube <andreas.taube@codecentric.de>
2016-01-07 21:40:38 +0100

    added slideshow

--- layouts/theme.config.php
+++ layouts/theme.config.php
@@ -119,6 +119,7 @@ $this['asset']->addFile('js', 'warp:vendor/uikit/js/components/search.js');
 $this['asset']->addFile('js', 'warp:vendor/uikit/js/components/sticky.js');
 $this['asset']->addFile('js', 'warp:vendor/uikit/js/components/tooltip.js');
 $this['asset']->addFile('js', 'warp:vendor/uikit/js/components/lightbox.js');
+$this['asset']->addFile('js', 'warp:vendor/uikit/js/components/slideshow.js');
 $this['asset']->addFile('js', 'js:social.js');
 $this['asset']->addFile('js', 'js:theme.js');
 
